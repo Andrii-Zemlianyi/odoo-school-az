9ab60582f8f0261655eda5fb0994426e580aa04a
Andrii Zemlianyi <andrey.zemlyanoy@gmail.com>
2022-07-09 16:51:28 +0300

    [IMP]  school_lesson_1_5: add demo functionality

--- school_lesson_1_5/models/models.py
+++ school_lesson_1_5/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class school_lesson_1_5(models.Model):
-#     _name = 'school_lesson_1_5.school_lesson_1_5'
-#     _description = 'school_lesson_1_5.school_lesson_1_5'
+class school_lesson_1_5(models.Model):
+    _name = 'school_lesson_1_5.school_lesson_1_5'
+    _description = 'school_lesson_1_5.school_lesson_1_5'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
