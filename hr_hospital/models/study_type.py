75f73528d673608c90c3a2c82477db385a349f64
Andrii Zemlianyi <andrey.zemlyanoy@gmail.com>
2022-07-24 21:09:21 +0300

    [IMP] simple views

--- hr_hospital/models/study_type.py
+++ hr_hospital/models/study_type.py
@@ -6,13 +6,8 @@ class StudyType(models.Model):
     _description = 'Type of study'
     _parent_name = "parent_id"
     _parent_store = True
-    #_rec_name = 'complete_name'
-    #_order = 'complete_name'
 
     name = fields.Char('Name', index=True, required=True)
-    # complete_name = fields.Char('Complete Name',
-    #                             compute='_compute_complete_name',
-    #                             store=True)
     parent_id = fields.Many2one('study.type', 'Parent Type', index=True,
                                 ondelete='cascade')
     parent_path = fields.Char(index=True)
