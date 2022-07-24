efabedd313566f6d1a790cf22bf6a82118d34966
Andrii Zemlianyi <andrey.zemlyanoy@gmail.com>
2022-07-24 21:15:12 +0300

    [IMP] manifest

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': """Hospital""",
 
     'author': "Andrii Zemlianyi",
-    # 'website': "http://odoo.school",
+    # 'website': "https://odoo.school",
 
     'category': 'Human Resources',
     'version': '15.0.1.0.2',
@@ -14,14 +14,22 @@
     'data': [
         'security/ir.model.access.csv',
         'views/hr_hospital_menus.xml',
-        'views/diagnosis_views.xml',
+        'views/contact_person_views.xml',
+        'views/disease_type_views.xml',
         'views/doctor_views.xml',
+        'views/patient_study_views.xml',
+        'views/personal_doctor_history_views.xml',
+        'views/study_type_views.xml',
+        'views/diagnosis_views.xml',
+        'views/disease_views.xml',
+        'views/hr_hospital_menus.xml',
         'views/patient_views.xml',
+        'views/sample_type_views.xml',
     ],
 
-    'demo': [
-        'demo/hr_hospital_demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/hr_hospital_demo.xml',
+    # ],
     'license': 'LGPL-3',
     'installable': True,
     'auto_install': False,
