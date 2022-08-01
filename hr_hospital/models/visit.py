641d580b8f3c167d3302bc18f3d5ec72048cee12
Andrii Zemlianyi <andrey.zemlyanoy@gmail.com>
2022-08-01 10:19:35 +0300

    [IMP] Calendar

--- hr_hospital/models/visit.py
+++ hr_hospital/models/visit.py
@@ -1,3 +1,4 @@
+import datetime
 from odoo import api, fields, models
 
 
@@ -9,8 +10,15 @@ class Visit(models.Model):
                                 index=True, required=True)
     patient_id = fields.Many2one('patient', string='Patient',
                                  index=True, required=True)
-    date_of_visit = fields.Date('Date of visit', required=True)
-    time_reception = fields.Integer('Time reception')
+    date_of_visit = fields.Datetime('Date of visit', required=True)
+    time_reception = fields.Integer('Time reception', required=True)
+    end_of_visit =  fields.Datetime('End of visit', compute='_compute_end_of_visit')
     study_ids = fields.Many2many('patient.study', string='Studies')
     diagnosis_id = fields.Many2one('diagnosis', string='Diagnosis')
     recommendation = fields.Text('Recommendation', required=True)
+
+    @api.depends('date_of_visit','time_reception')
+    def _compute_end_of_visit(self):
+        for rec in self:
+            rec.end_of_visit = rec.date_of_visit + \
+                datetime.timedelta(minutes=rec.time_reception)
